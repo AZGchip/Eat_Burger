0549c1ba053e8a06497b4a17acbb7256ed290a94
AZGchip <60306563+AZGchip@users.noreply.github.com>
2020-05-04 14:14:35 -0400

    created database files, package.json , and server,js files. added database setup to schema.sql. added added three starting rows to seeds.sql.

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE burgereat;
+USE burgereat;
+CREATE TABLE burger
+(
+	burger_id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(60) NOT NULL,
+    eaten BOOLEAN,
+	PRIMARY KEY (burger_id)
+    );
---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+USE burgereat;
+
+INSERT INTO burger
+(burger_name,eaten) 
+VALUES 
+("cheese borger",false);
+
+INSERT INTO burger
+(burger_name,eaten) 
+VALUES 
+("bees churger",false);
+
+INSERT INTO burger
+(burger_name,eaten) 
+VALUES 
+("nothing burger",false);
